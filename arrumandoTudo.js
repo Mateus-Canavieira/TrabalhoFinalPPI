d8b8a23506d1b446480f9538b3d268f6b0aa47c9
Mateus-Canavieira <72403136+Mateus-Canavieira@users.noreply.github.com>
2023-10-16 19:41:36 -0300

    atualização mesmo kkkkkk
    19:40

---
+++ arrumandoTudo.js
@@ -0,0 +1,6 @@
+
+    // Adiciona um ouvinte de evento para o botão de login
+    function login() {
+        document.getElementById('botaoEscondido').classList.remove("escondido");
+        
+    }
